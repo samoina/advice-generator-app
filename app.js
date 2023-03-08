9c46e5823f621864cbdf42962aa89b4b4fe99344
Samoina <samoina.w@gmail.com>
2023-03-08 19:24:01 +0300

    Use fetch() in Javascript to show the advice

--- app.js
+++ app.js
@@ -8,12 +8,26 @@ const mainButton = document.querySelector('.main__button'),
 //add Event Listener to the button on click
 mainButton.addEventListener('click', loadAdvice);
 
-async function loadAdvice() {
-  let res = await fetch(url);
-  let data = await res.json();
+//ASYNC AWAIT
+// async function loadAdvice() {
+//   let res = await fetch(url);
+//   let data = await res.json();
 
-  mainPara.textContent = data.slip.advice;
-  adviceNum.textContent = `#${data.slip.id}`;
+//   mainPara.textContent = data.slip.advice;
+//   adviceNum.textContent = `#${data.slip.id}`;
+// }
+
+//FETCH()
+function loadAdvice() {
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    mainPara.textContent = `"${data.slip.advice}"`;
+    adviceNum.textContent = `#${data.slip.id}`;
+
+
+})
 }
 
+document.onload = loadAdvice();
 

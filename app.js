991169ee01e2775be557d0f63903259b6cf3abc3
Samoina <samoina.w@gmail.com>
2023-03-09 10:22:12 +0300

    Practice and use XHR object to fetch the advice

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //Declare constants
 const mainButton = document.querySelector('.main__button'),
-      adviceNum = document.querySelector('.adviceNum'),
-      mainPara = document.querySelector('.main__para'),
-      url = 'https://api.adviceslip.com/advice';
+  adviceNum = document.querySelector('.adviceNum'),
+  mainPara = document.querySelector('.main__para'),
+  url = 'https://api.adviceslip.com/advice';
 
 
 //add Event Listener to the button on click
@@ -18,16 +18,39 @@ mainButton.addEventListener('click', loadAdvice);
 // }
 
 //FETCH()
-function loadAdvice() {
-  fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    mainPara.textContent = `"${data.slip.advice}"`;
-    adviceNum.textContent = `#${data.slip.id}`;
+// function loadAdvice() {
+//   fetch(url)
+//     .then(res => res.json())
+//     .then(data => {
+//       mainPara.textContent = `"${data.slip.advice}"`;
+//       adviceNum.textContent = `#${data.slip.id}`;
+//     })
+// }
 
+// CALLBACKS & XHR
+function loadAdvice(){
+  const xhr = new XMLHttpRequest();
 
-})
+  xhr.open('GET', url, true);
+  xhr.send();
+  xhr.onload = function(){
+    if(this.status === 200) {
+      let adviceObj = JSON.parse(this.responseText);
+      console.log(adviceObj.slip.advice);
+      console.log(adviceObj.slip.id);
+      mainPara.textContent = `"${adviceObj.slip.advice}"`;
+      adviceNum.textContent = `#${adviceObj.slip.id}`;
+    } else {
+      let error = `Error: ${this.status}`
+      mainPara.textContent = `"${error}. Page not found"`;
+    }
+  }
 }
 
+
+
 document.onload = loadAdvice();
+
+
+
 
